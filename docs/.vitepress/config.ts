f353ded64443113d1ff72094d3828b34417dbb38
waioyou <291159817@qq.com>
2025-02-25 00:05:55 +0800

    chore: vitepress 中文设置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,7 @@ import { containerPreview, componentPreview } from '@vitepress-demo-preview/plug
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: 'U Element Plus',
-  head: [['link', { rel: 'icon', href: '/public/favicon.ico' }]],
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   description: '基于Element Plus的二次封装,实现如表格组件,表单组件等功能.',
   vite: {
     resolve: {
@@ -26,7 +26,20 @@ export default defineConfig({
       md.use(containerPreview)
       md.use(componentPreview)
     },
+    //显示行数
+    lineNumbers: true,
+    //中文配置
+    container: {
+      tipLabel: '提示',
+      warningLabel: '警告',
+      noteLabel: '注意',
+      dangerLabel: '危险',
+      detailsLabel: '详情',
+      infoLabel: '信息',
+    },
   },
+  lang: 'zh-CN',
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -46,6 +59,43 @@ export default defineConfig({
       },
     ],
 
+    // 添加外观切换的中文设置
+    darkModeSwitchLabel: '切换主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+    sidebarMenuLabel: '菜单',
+
+    // 文档页脚
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+    // 大纲标题
+    outlineTitle: '本页目录',
+    // 返回顶部按钮文字
+    returnToTopLabel: '返回顶部',
+    // 社交链接
     socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+    // 搜索
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+              closeText: '关闭',
+            },
+          },
+        },
+      },
+    },
   },
 })
