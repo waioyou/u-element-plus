8136dd62a7b2bb9e331c4d22b1b438ec7c625ada
waioyou <291159817@qq.com>
2025-03-18 13:38:57 +0800

    docs(operation): 示例使用useTable优化

--- docs/demo/operation/type.vue
+++ docs/demo/operation/type.vue
@@ -1,38 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useTableData } from '../table/useTableData'
-import { ElMessage } from 'element-plus'
-import type { TableColumns, Operations } from 'u-element-plus'
-import type { TableDataItem } from '../table/useTableData'
+import { onMounted, ref } from 'vue'
+import { ElTag, ElMessage } from 'element-plus'
+import { useTable } from 'u-element-plus'
+import { dicts, getDictType, getDictText, getUserList } from '@docs/mock/user'
+import type { User } from '@docs/mock/types'
 
-const { tableData, statusOptions, getOptionText, getOptionType } = useTableData(6, true)
-
-// 操作按钮列表
-const operations = ref<Operations<TableDataItem>>([
-  // 根据状态判断是否显示编辑按钮
-  { label: '编辑', name: 'edit', if: (row) => row.status === '1' },
-  {
-    label: '删除',
-    name: 'delete',
-    buttonAttrs: () => ({
-      type: 'danger',
-    }),
-    confirmAttrs: (row, index) => ({
-      title: `确定删除第【${index + 1}】条数据【${row.name}】吗？`,
-      width: 170,
-    }),
+const { tableData, tableColumns, tableOperations, setTableColumns, setTableOperations } = useTable(
+  async () => {
+    const res = await getUserList(10)
+    return res.data
   },
-  {
-    label: '详情',
-    name: 'detail',
-    buttonAttrs: () => ({
-      type: 'primary',
-    }),
-  },
-])
+)
 
 // 点击操作按钮
-const handleClickOperation = (name: string, row: TableDataItem, index: number) => {
+const handleClickOperation = (name: string, row: User, index: number) => {
   if (name === 'delete') {
     ElMessage.error(`点击了第${index + 1}条数据【${row.name}】的【${name}】操作`)
   } else {
@@ -42,52 +23,77 @@ const handleClickOperation = (name: string, row: TableDataItem, index: number) =
 
 const operationType = ref<'button' | 'link' | 'text'>('text')
 
-const tableColumns = ref<TableColumns<TableDataItem>>([
-  { prop: 'id', label: '编号', width: 110, align: 'left', sortable: true },
-  { prop: 'name', label: '姓名', minWidth: 90, align: 'center' },
-  {
-    prop: 'gender',
-    label: '性别',
-    width: 60,
-    align: 'center',
-    formatter: ({ row }) => (row.gender === '1' ? '男' : '女'),
-  },
-  { prop: 'birthday', label: '出生日期', width: 110, align: 'center', sortable: true },
-  { prop: 'degree', label: '学历', width: 60, align: 'center' },
-  { prop: 'school', label: '毕业学校', minWidth: 120, align: 'center' },
-  { prop: 'major', label: '专业', minWidth: 138, align: 'left' },
-  { prop: 'graduationYear', label: '毕业年份', width: 100, align: 'center' },
-  {
-    prop: 'status',
-    label: '状态',
-    width: 80,
-    align: 'center',
-    filters: statusOptions.map((d) => ({ text: d.label, value: d.value })),
-    filterMethod: (value: any, row: any, column: any) => {
-      const property = column['property']
-      return row[property] === value
+onMounted(() => {
+  setTableOperations([
+    // 根据状态判断是否显示编辑按钮
+    { label: '编辑', name: 'edit', if: (row) => row.status === '1' },
+    {
+      label: '删除',
+      name: 'delete',
+      buttonAttrs: () => ({
+        type: 'danger',
+      }),
+      confirmAttrs: (row, index) => ({
+        title: `确定删除第【${index + 1}】条数据【${row.name}】吗？`,
+        width: 170,
+      }),
     },
-  },
-  {
-    prop: 'address',
-    label: '地址',
-    minWidth: 200,
-    align: 'left',
-    showOverflowTooltip: true,
-    formatter: ({ row }) => {
-      return `${row.province}${row.city}${row.district}${row.address}`
+    {
+      label: '详情',
+      name: 'detail',
+      buttonAttrs: () => ({
+        type: 'primary',
+      }),
     },
-  },
-  {
-    prop: 'operation',
-    label: '操作',
-    width: 180,
-    align: 'center',
-    fixed: 'right',
-    component: 'operation',
-    attrs: { type: operationType, operations },
-  },
-])
+  ])
+
+  setTableColumns([
+    { prop: 'id', label: '编号', width: 110, align: 'left', sortable: true },
+    { prop: 'name', label: '姓名', minWidth: 90, align: 'center' },
+    {
+      prop: 'gender',
+      label: '性别',
+      width: 60,
+      align: 'center',
+      formatter: ({ row }) => (row.gender === '1' ? '男' : '女'),
+    },
+    { prop: 'birthday', label: '出生日期', width: 110, align: 'center', sortable: true },
+    { prop: 'degree', label: '学历', width: 60, align: 'center' },
+    { prop: 'school', label: '毕业学校', minWidth: 120, align: 'center' },
+    { prop: 'major', label: '专业', minWidth: 138, align: 'left' },
+    { prop: 'graduationYear', label: '毕业年份', width: 100, align: 'center' },
+    {
+      prop: 'status',
+      label: '状态',
+      width: 80,
+      align: 'center',
+      filters: dicts.status.map((d) => ({ text: d.label, value: d.value })),
+      filterMethod: (value: any, row: any, column: any) => {
+        const property = column['property']
+        return row[property] === value
+      },
+    },
+    {
+      prop: 'address',
+      label: '地址',
+      minWidth: 200,
+      align: 'left',
+      showOverflowTooltip: true,
+      formatter: ({ row }) => {
+        return `${row.province}${row.city}${row.district}${row.address}`
+      },
+    },
+    {
+      prop: 'operation',
+      label: '操作',
+      width: 180,
+      align: 'center',
+      fixed: 'right',
+      component: 'operation',
+      attrs: { type: operationType, operations: tableOperations },
+    },
+  ])
+})
 </script>
 
 <template>
@@ -105,8 +111,8 @@ const tableColumns = ref<TableColumns<TableDataItem>>([
     @click-operation="handleClickOperation"
   >
     <template #status="{ row }">
-      <el-tag :type="getOptionType(statusOptions, row.status)">
-        {{ getOptionText(statusOptions, row.status) }}
+      <el-tag :type="getDictType(dicts.status, row.status)">
+        {{ getDictText(dicts.status, row.status) }}
       </el-tag>
     </template>
   </u-table>
